c5c523574e83538c6a36c08da378b2f877b85729
soubhikdas <d.soubhik@hotmail.com>
2025-01-23 10:51:25 +0530

    Update sample-test.js

--- js/sample-test.js
+++ js/sample-test.js
@@ -21,7 +21,7 @@ function getSampleExams() {
 
   n2Elem.innerHTML =
     "<center><div class='headsmptst'>" +
-    "<span><form id='submtsmptst'><input id='srchsamtest' type='text' class='form-control' placeholder='Search Exam (Not Available! 📢 2025 Update)' required disabled><input type='submit' value='Go' id='submtbtnsmp'></form></span>" +
+    "<span><form id='submtsmptst'><input id='srchsamtest' type='text' class='form-control' placeholder='MASTROWALL Sample Exams' required disabled><input type='submit' value='Go' style='display:none;' id='submtbtnsmp'></form></span>" +
     "<span class='closesmprtl'>Close</span></div></center>";
 
   pElem.appendChild(n2Elem);
